9badbbc2f67bfa9e91d630e8181b83d64727d303
Sagittta <js1verj@gmail.com>
2019-10-17 16:05:16 +0900

    splash screen, colors, styles, icon change

--- app/src/main/java/kr/hs/emirim/lyn/failydaily/SplashActivity.java
+++ app/src/main/java/kr/hs/emirim/lyn/failydaily/SplashActivity.java
@@ -2,15 +2,17 @@ package kr.hs.emirim.lyn.failydaily;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
-
-    UseDB user;
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
