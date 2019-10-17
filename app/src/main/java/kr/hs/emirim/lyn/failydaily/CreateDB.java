c1b0e51fc24892a029e242b8dcd658b6278f2d39
yeLyn36 <s2018w11@e-mirim.hs.kr>
2019-10-17 21:40:02 +0900

    modify create table query

--- app/src/main/java/kr/hs/emirim/lyn/failydaily/CreateDB.java
+++ app/src/main/java/kr/hs/emirim/lyn/failydaily/CreateDB.java
@@ -15,7 +15,7 @@ public final class CreateDB {
         public static final String name = "name";
         public static final String age = "age";
         public static final String num = "num";
-        
+
         public static final String _TABLENAME1 = "diaryDB";
         public static final String _TABLENAME2 = "FairyDB";
 
@@ -25,13 +25,13 @@ public final class CreateDB {
                 +subtitle+" text not null , "
                 +content+" text not null , "
                 +category+" text not null , "
-                +date+" text not null DEFAULT (datetime('now', 'localtime'))";
+                +date+" text not null DEFAULT (datetime('now', 'localtime')))";
 
         public static final String _CREATE2 = "create table if not exists "+_TABLENAME2+"("
                 +_ID+" integer primary key autoincrement, "
                 +name+" text not null , "
                 +age+" integer not null , "
                 +category+" text not null , "
-                +num+" integer not null";
+                +num+" integer not null)";
     }
 }
