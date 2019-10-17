fb3aa972bf735213d102b76eb7bacd7f89f3c797
LHyunSoo <s2018w36@e-mirim.hs.kr>
2019-10-17 23:19:43 +0900

    setStatusBarColor

--- app/src/main/java/kr/hs/emirim/lyn/failydaily/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/lyn/failydaily/MainActivity.java
@@ -1,7 +1,11 @@
 package kr.hs.emirim.lyn.failydaily;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +15,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View view = getWindow().getDecorView();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (view != null) {
+                // 23 버전 이상일 때 상태바 하얀 색상에 회색 아이콘 색상을 설정
+                view.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+                getWindow().setStatusBarColor(Color.parseColor("#efb951"));
+            }
+        }else if (Build.VERSION.SDK_INT >= 21) {
+            // 21 버전 이상일 때
+            getWindow().setStatusBarColor(Color.parseColor("#efb951"));
+        }
+
         user = new UseDB(this);
         user.open();
 //        user.create();
